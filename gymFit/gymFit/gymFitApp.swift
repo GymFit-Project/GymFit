ae1353438f8bd7486ecffc11e1a22ece9b9923d5
Fabio Hernandez <fahio512@gmail.com>
2023-11-05 10:34:04 -0500

    init

---
+++ gymFit/gymFit/gymFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymFitApp.swift
+//  gymFit
+//
+//  Created by Fabio on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct gymFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
