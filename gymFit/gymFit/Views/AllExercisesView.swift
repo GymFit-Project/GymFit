162b566cb47a5961bb5d0902cfc1ebcd90c7a53d
Anthony Khera <anthonykhera@yahoo.com>
2023-11-05 23:45:48 -0500

    Began AllExerciseView

---
+++ gymFit/gymFit/Views/AllExercisesView.swift
@@ -0,0 +1,18 @@
+//
+//  AllExercisesView.swift
+//  gymFit
+//
+//  Created by Anthony Khera on 11/5/23.
+//
+
+import SwiftUI
+
+struct AllExercisesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllExercisesView()
+}
