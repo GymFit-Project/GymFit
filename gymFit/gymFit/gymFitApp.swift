3ae74ca4b13731000d7af8cf2761d948bdaece10
Fabio Hernandez <fahio512@gmail.com>
2023-11-20 16:04:35 -0500

    back4back initial setup

--- gymFit/gymFit/gymFitApp.swift
+++ gymFit/gymFit/gymFitApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import ParseSwift
 
 @main
 struct gymFitApp: App {
+    init() {
+        ParseSwift.initialize(applicationId: "8m9cY1kjVuDvV1vlVW9wx0J6ULjyMNvFVacBHn0L",
+                                      clientKey: "1z3WRkB35nDS8BB7iGxC2cnPXi0wjjd4E6ODVrA1",
+                                      serverURL: URL(string: "https://parseapi.back4app.com")!)
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
